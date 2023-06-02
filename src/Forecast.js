fdae7faf8af5f2939062c0fa376bbe48774ad7e9
ZoeSttn <98645467+ZoeSttn@users.noreply.github.com>
2023-06-02 11:34:44 +0200

    Added icon, temperatures and name to forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -6,12 +6,22 @@ export default function Forecast(props) {
   let date = new Date(props.forecast.time * 1000);
   let weekday = week[date.getDay()];
 
-  console.log(props.forecast.temperature);
+  let maxtemp = Math.round(props.forecast.temperature.maximum);
+  let mintemp = Math.round(props.forecast.temperature.minimum);
+
+  let icon = props.forecast.condition.icon_url;
+
+  console.log(props.forecast);
 
   return (
     <div className="col-sm">
       <div>{weekday}</div>
-      <div></div>
+      <div>
+        {maxtemp} | {mintemp}
+      </div>
+      <div>
+        <img src={icon} />
+      </div>
     </div>
   );
 }
